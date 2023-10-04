4cb0887e05f75e232031ce299419ca9f4cd7ce95
emraho <emraho@localhost>
2023-10-04 16:58:40 -0500

    studio 4 done

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,12 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+		StdDraw.setPenRadius(.005);
+		StdDraw.rectangle(.5,.5,.3,.2);
+		StdDraw.setPenColor(255, 0, 0);
+		StdDraw.filledCircle(.5, .5, .1);
+		StdDraw.setPenColor(0, 0, 255);
+		StdDraw.filledRectangle(.5,.5,.05,.05);
+		
 	}
 }
--- src/studio4/InterpretDrawingFile.java
+++ src/studio4/InterpretDrawingFile.java
@@ -19,6 +19,28 @@ public class InterpretDrawingFile {
 		chooser.showOpenDialog(null);
 		File f = new File(chooser.getSelectedFile().getPath());
 		Scanner in = new Scanner(f); //making Scanner with a File
-		
+			String shapeType = in.next();
+			int redComponent = in.nextInt();
+			int greenComponent = in.nextInt();
+			int blueComponent = in.nextInt();
+			boolean isFilled = in.nextBoolean();
+			double parameterone = in.nextDouble();
+			double parametertwo = in.nextDouble();
+			double parameterthree = in.nextDouble();
+			double parameterfour = in.nextDouble();
+			if (shapeType.equals("triangle")) {
+				double parameterfive = in.nextDouble();
+				double parametersix = in.nextDouble();
+			
+			}
+			if (shapeType.equals("rectangle")) {
+				StdDraw.rectangle(parameterone, parametertwo, parameterthree, parameterfour);
+			}
+			if (shapeType.equals("ellipse")) {
+				StdDraw.ellipse(parameterone, parametertwo, parameterthree, parameterfour);
+			}
+			
 	}
+		
 }
+
